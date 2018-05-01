ceb9d122ebb60d7c4c84518fbd3460accd2c1913
SmithsNZ <smiths.nz@gmail.com>
2018-05-01 16:01:23 +1200

    Test

---
+++ GitSync.sh
@@ -0,0 +1,25 @@
+echo
+echo full directory
+
+ls -la
+
+if [ "$#" -eq  "0" ]
+	then
+		echo "Please run with commit message"
+		exit
+fi
+
+echo
+echo Committing local changes
+git add *
+git commit -a -m $1
+
+echo
+echo Fetching master and merging with local, remote wins if conflict
+
+git fetch origin master
+git merge -s recursive -X theirs origin/master
+
+echo
+echo show any local files not on remote master
+git clean -dn
